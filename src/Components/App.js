b014128e7ba1666136f81e968f0fda929fc65205
Chris King <cking5836@gmail.com>
2020-06-10 18:49:08 -0400

    Finally getting some data

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from './Header';
+import Container from './Container';
 
 // Importing API link and key
 import { BASE_URL, API_KEY } from '../secrets';
 import Axios from "axios";
 
 function App() {
-  Axios.get(`http:${BASE_URL}?${API_KEY}`)
+  const[photo,setPhoto] = useState();
+  const[title,setTitle] = useState();
+  const[info,setInfo] = useState();
+  const[date,setDate] = useState();
+
+  useEffect(() => {
+    Axios.get(`http:${BASE_URL}?${API_KEY}`)
     .then(response => {
-      console.log(response.data)
+      const dailyImg = response.data.url
+      setPhoto(dailyImg)
+      const dailyTitle = response.data.title
+      setTitle(dailyTitle)
     })
+  })
 
   return (
     <div className="App">
       <Header />
+      <img src={photo} />
+      <h2>{title}</h2>
     </div>
   );
 }
--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// Importing API link and key
+import { BASE_URL, API_KEY } from '../secrets';
+import Axios from "axios";
 
-export default function Container(props) => {
-    return null
+export default function Container(props) {
 }
